1ef51db6c9d32ecac2f2542b152bd37485a076b8
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-04 18:11:06 -0300

    agregado de animacion en landing, correccion de animaciones en cards home, agregado seccion about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function About() {
     return (
         <div>
-            <h2>Descripcion sobre el proyecto</h2>            
+            <h2>Gracias por llegar hasta aqui!</h2>
+            <p>Esta aplicación es un proyecto individual que fue creado durante mi bootcamp en <a href="https://www.soyhenry.com" target="_blank" rel="noopener noreferrer">Soy Henry</a>, siendo uno de los 2 proyectos requeridos.</p>
+            <p>Las tecnologías utilizadas aqui son PostgreSQL, Sequelize, Express, React y Redux, entre otras. Esta aplicación se sirve de la API <a href="https://restcountries.eu/" target="_blank" rel="noopener noreferrer">Rest Countries</a> para obtener información sobre todos los paises del mundo, y luego almacenarlos para trabajarlos y requerirlos desde la API propia.</p>            
+            <h3>Contacto</h3>
+            <div>
+                aaaaaaaaa perro ehhh
+            </div>
+
         </div>
     )
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 import logoHenry from '../img/logoHenry.png'
 import './Nav.css'
 
@@ -7,11 +7,14 @@ export default function Nav() {
     return (
         <div className='nav'>
             <div className='nav-img'>
-                <img src={logoHenry} alt="" />
+                <a href="https://www.soyhenry.com" target="_blank" rel="noopener noreferrer">
+                    <img src={logoHenry} alt="" />
+                </a>
             </div>
             <div className='nav-links'>
                 <NavLink to="/home" activeClassName="nav-link-active" className='nav-link'>HOME</NavLink>
                 <NavLink to="/activity" activeClassName="nav-link-active" className='nav-link'>ACTIVIDAD</NavLink>
+                <NavLink to="/about" activeClassName="nav-link-active" className='nav-link'>ABOUT</NavLink>
             </div>
             <span className='nav-text'>Proyecto Individual FT13</span>
         </div>
