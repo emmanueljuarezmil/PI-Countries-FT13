b6c831b063c8d5a43de7280b654c226a39e17138
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-08-17 23:29:00 -0300

    pre carousel

--- client/src/components/ActivityCard/ActivityCard.jsx
+++ client/src/components/ActivityCard/ActivityCard.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 const ActivityCard = ({activity}) => {
+    console.log(activity)
+    const {
+        name,
+        description,
+        difficult,
+        duration,
+        // countries
+    } = activity
+
+    const difficults = [
+        'Muy fácil',
+        'Fácil',
+        'Intermedia',
+        'Dificil',
+        'Muy dificil',
+    ]
+
+    
     return (
         <div>
-            {activity.name}
+            <p>{name}</p>
+            <p>{description}</p>
+            <p>{difficults[difficult - 1]}</p>
+            <p>{duration}</p>            
         </div>
     )
 }
