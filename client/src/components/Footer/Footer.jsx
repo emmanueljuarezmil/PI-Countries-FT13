7bda6ac30cb320b122a667d2750cd6b30e7cd225
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-10-15 13:53:10 -0300

    footer content

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import './Footer.css'
 import React from 'react'
+import express from '../../img/Express logo.png'
+import postgres from '../../img/postgres logo.png'
+import react from '../../img/react logo.png'
+import redux from '../../img/redux logo.png'
+import sequelize from '../../img/sequelize.png'
 
 const Footer = () => {
     return (
-        <div>
-            ola soy el footer
+        <div className='footer'>
+            <div className='footer-element footer-poweredby'>
+                <span>
+                    MADE WITH:
+                </span>
+                <img src={express} className='footer-icon' alt="Logo Express.js"/>
+                <img src={postgres} className='footer-icon' alt="Logo PostgreSQL"/>
+                <img src={react} className='footer-icon' alt="Logo React"/>
+                <img src={redux} className='footer-icon' alt="Logo Redux"/>
+                <img src={sequelize} className='footer-icon' alt="Logo Sequelize"/>
+            </div>
+            <span>2021</span>
         </div>
     )
 }
